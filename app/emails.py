8abe9b55a592f44322862d37dc7b762ebd770358
sirius226 <394544256@qq.com>
2014-07-27 22:54:32 -0400

    add Asynchronous call decorator for mail support

--- app/emails.py
+++ app/emails.py
@@ -1,14 +1,18 @@
 from flask import render_template
 from flask.ext.mail import Message
 from app import mail
-## from decorators import async
 from config import ADMINS
+from decorators import async
+
+@async
+def send_async_email(msg):
+    mail.send(msg)
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender = sender, recipients = recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    send_async_email(msg)
 
 def follower_notification(followed, follower):
     send_email("[microblog] %s is now following you!" % follower.nickname,
